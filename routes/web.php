bde09e4323b1d9a9f4c584d1090ac397961f30c5
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-03-05 23:50:16 +0700

    Create a route.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Fauzan",
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', [
+        "name" => "Muhammad Fauzan",
+        "email" => "fauzan@gmail.com",
+        "phone" => "08123456789",
+    ]);
+});
+
+Route::get('/login', function () {
+    return view('login', [
+        "login" => "Login Page",
+    ]);
 });
