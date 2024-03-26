3c80422b1c6e1345437d23968078b6d46312017a
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-03-26 20:52:10 +0700

    Slightly changes in file resources.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,11 @@
 @extends('layout.main')
+
 @section('content')
 
     <h1>Restaurant List</h1>
-        @foreach ($restaurant as $r)
-            <h1>{{$r['name']}}</h1>
-            <h1>{{$r['name']}}</h1>
-        @endforeach
+    @foreach ($restaurant as $r)
+        <h1>{{$r['name']}}</h1>
+        <p>{{$r['description']}}</p>
+    @endforeach
 
-@endsection
-
-@section('footer')
 @endsection
