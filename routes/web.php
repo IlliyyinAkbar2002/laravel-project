8bcc126d331a81b3fb78b5a7eda57e0dd169417f
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-03-26 20:48:48 +0700

    Slightly modifying routes.

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ use Illuminate\Support\Facades\Route;
 //     ]);
 // });
 
+Route::get('/', function () {
+    return view('index');
+});
+
 Route::get('/index', function () {
     return view('index');
 });
