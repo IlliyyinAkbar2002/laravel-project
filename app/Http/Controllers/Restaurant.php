85dff5c53b4054b778b686379a9a387dc44268b4
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-03-31 11:02:13 +0700

    Create Controller name Restaurant and create method 'SelectDataRestaurant' to view data on select.

--- app/Http/Controllers/Restaurant.php
+++ app/Http/Controllers/Restaurant.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 class Restaurant extends Controller
 {
     //
-    
+
+    public function SelectDataRestauran($select)
+    {
+        $data_restaurant = \App\Models\Restaurant::SelectRestaurant($select);
+        return view('contact', [
+            "title" => "Restaurant",
+            "data_all" => $data_restaurant
+        ]);
+    }
+
 }
