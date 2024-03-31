ef20267518a51096455a4f4cc44344b35040d3da
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-03-31 15:49:55 +0700

    Create 2 methods.

--- app/Http/Controllers/RestaurantControllerData.php
+++ app/Http/Controllers/RestaurantControllerData.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\NewRestaurant; // Ensure correct model is used
 
 class RestaurantControllerData extends Controller
 {
-    
+    public function index()
+    {
+        $restaurants = NewRestaurant::all(); // Correct model name
+        return view('restaurants', ['restaurants' => $restaurants]); // Correct view path
+    }
+
+    public function show($id)
+    {
+        $restaurant = NewRestaurant::find($id); // Correct model name
+        return view('restaurants.show', ['restaurant' => $restaurant]); // Assuming 'restaurants.show' is a correct view path
+    }
 }
