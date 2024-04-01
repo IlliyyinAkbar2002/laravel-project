8ae6399cf9b3e81f3a1820fb55d7c9d349a39b91
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-04-01 15:59:22 +0700

    Slightly changes.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
-    <h1>Restaurant List</h1>
+<div class="p-5 mb-4 bg-body-tertiary rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">CRUD Operations</h1>
+        <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+    </div>
+</div>
+
+
 
 @endsection
